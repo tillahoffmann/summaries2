1839150422b86e575bae4f16430030fbe7963632
Till Hoffmann <tillahoffmann@gmail.com>
2023-06-25 12:34:09 -0400

    Add script to train transformers for the coalescent model.

---
+++ summaries/experiments/coal.py
@@ -0,0 +1,74 @@
+from torch import nn, Tensor
+from torch.distributions import AffineTransform, Beta, Categorical, Independent, \
+    MixtureSameFamily, TransformedDistribution
+
+from ..transformers import NeuralTransformer
+
+
+class _CoalTransformer(NeuralTransformer):
+    """
+    Learnable transformer for the coalescent problem without the "head" of the network.
+    """
+    def __init__(self) -> None:
+        transformer = nn.Sequential(
+            nn.LazyLinear(16),
+            nn.Tanh(),
+            nn.LazyLinear(16),
+            nn.Tanh(),
+            nn.LazyLinear(2),
+            nn.Tanh(),
+        )
+        super().__init__(transformer)
+
+
+class CoalPosteriorMeanTransformer(_CoalTransformer):
+    """
+    Learnable transformer with posterior mean predictive "head".
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self.predictor = nn.Sequential(
+            nn.LazyLinear(16),
+            nn.Tanh(),
+            nn.LazyLinear(2),
+        )
+
+    def forward(self, data: Tensor) -> Tensor:
+        transformed = self.transform(data)
+        return self.predictor(transformed)
+
+
+class CoalPosteriorMixtureDensityTransformer(_CoalTransformer):
+    """
+    Learnable transformer with conditional posterior density estimation "head" based on mixture
+    density networks.
+    """
+    def __init__(self, n_components: int = 10) -> None:
+        super().__init__()
+        self.n_components = n_components
+        self.mixture_parameters = nn.ModuleDict({
+            key: nn.Sequential(
+                nn.LazyLinear(16),
+                nn.Tanh(),
+                nn.LazyLinear(size * n_components),
+            ) for (size, key) in [(1, "logits"), (2, "concentration1s"), (2, "concentration0s")]
+        })
+
+    def forward(self, data: Tensor) -> Tensor:
+        transformed = self.transform(data)
+
+        logits: Tensor = self.mixture_parameters["logits"](transformed)
+        mixture_dist = Categorical(logits=logits)
+
+        concentration1s: Tensor = self.mixture_parameters["concentration1s"](transformed)
+        concentration1s = concentration1s.reshape((-1, self.n_components, 2)).exp()
+        concentration0s: Tensor = self.mixture_parameters["concentration0s"](transformed)
+        concentration0s = concentration0s.reshape((-1, self.n_components, 2)).exp()
+        component_dist = Beta(concentration1s, concentration0s)
+
+        # Rescale the distribution to the [0, 10] domain used by the simulations and reinterpret the
+        # last batch dimension as an event dimension.
+        component_dist = TransformedDistribution(component_dist, AffineTransform(0, 10))
+        component_dist = Independent(component_dist, 1)
+
+        return MixtureSameFamily(mixture_dist, component_dist)
---
+++ summaries/scripts/train_transformer.py
@@ -0,0 +1,136 @@
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from datetime import datetime
+import itertools as it
+import numpy as np
+from pathlib import Path
+import pickle
+from torch import as_tensor, get_default_dtype, nn, no_grad, Tensor
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import DataLoader, TensorDataset
+from tqdm import tqdm
+from typing import Any, Callable, Dict, List, Optional, Type
+
+from ..experiments.coal import CoalPosteriorMixtureDensityTransformer, CoalPosteriorMeanTransformer
+from ..nn import NegLogProbLoss
+from ..transformers import NeuralTransformer
+from .base import resolve_path
+
+
+class Args:
+    config: str
+    train: Path
+    validation: Path
+    output: Path
+
+
+@dataclass
+class TrainConfig:
+    loss: Callable[..., Tensor]
+    transformer_cls: Type[NeuralTransformer]
+    transformer_kwargs: Dict[str, Any] | None = None
+    max_epochs: int | None = None
+
+
+TRAIN_CONFIGS = {
+    "coal-neural_posterior_mean": TrainConfig(
+        nn.MSELoss(),
+        CoalPosteriorMeanTransformer,
+    ),
+    "coal-mixture_density_network": TrainConfig(
+        NegLogProbLoss(),
+        CoalPosteriorMixtureDensityTransformer,
+    )
+}
+
+
+def _create_data_loader_from_pickle(path: Path, **kwargs) -> DataLoader:
+    with path.open("rb") as fp:
+        result = pickle.load(fp)
+    dtype = kwargs.pop("dtype", get_default_dtype())
+    data = as_tensor(result["data"], dtype=dtype)
+    params = as_tensor(result["params"], dtype=dtype)
+    dataset = TensorDataset(data, params)
+    return DataLoader(dataset, **kwargs)
+
+
+def __main__(argv: Optional[List[str]] = None) -> None:
+    start = datetime.now()
+    parser = ArgumentParser("train_transformer")
+    parser.add_argument("config", choices=TRAIN_CONFIGS, help="training configuration")
+    parser.add_argument("train", type=resolve_path, help="path to training data")
+    parser.add_argument("validation", type=resolve_path, help="path to validation data")
+    parser.add_argument("output", type=resolve_path, help="path to output file")
+    args: Args = parser.parse_args(argv)
+
+    # Load the data into tensor datasets.
+    data_loader_kwargs = {
+        "batch_size": 64,
+        "shuffle": True,
+    }
+    train_loader = _create_data_loader_from_pickle(args.train, **data_loader_kwargs)
+    validation_loader = _create_data_loader_from_pickle(args.validation, **data_loader_kwargs)
+
+    config = TRAIN_CONFIGS[args.config]
+    transformer = config.transformer_cls(**(config.transformer_kwargs or {}))
+
+    # Run one pilot batch to initialize the lazy modules.
+    data: Tensor
+    for data, _ in train_loader:
+        transformer(data)
+        break
+
+    # Run the training.
+    optim = Adam(transformer.parameters(), 1e-3)
+    scheduler = ReduceLROnPlateau(optim, verbose=True)
+
+    best_loss = np.inf
+    n_bad_epochs = 0
+    for epoch in tqdm(it.count(1), total=config.max_epochs):
+        # Run one epoch.
+        params: Tensor
+        for data, params in train_loader:
+            output = transformer(data)
+            loss_value = config.loss(output, params)
+            loss_value.backward()
+            optim.step()
+
+        # Evaluate the validation loss and update the learning rate.
+        sizes = []
+        loss_values = []
+        with no_grad():
+            for data, params in validation_loader:
+                sizes.append(data.shape[0])
+                output = transformer(data)
+                loss_value = config.loss(output, params)
+                loss_values.append(loss_value)
+
+        validation_loss = np.dot(sizes, loss_values) / np.sum(sizes)
+        scheduler.step(validation_loss)
+
+        # Break if we've reached the maximum number of epochs.
+        if config.max_epochs and epoch == config.max_epochs:
+            break
+
+        # Determine whether to stop training based on validation loss.
+        if validation_loss < best_loss:
+            best_loss = validation_loss
+            n_bad_epochs = 0
+        else:
+            n_bad_epochs += 1
+
+        if n_bad_epochs > 2 * scheduler.patience:
+            break
+
+    with args.output.open("wb") as fp:
+        pickle.dump({
+            "args": vars(args),
+            "start": start,
+            "end": datetime.now(),
+            "transformer": transformer,
+        }, fp)
+
+
+if __name__ == "__main__":
+    __main__()
