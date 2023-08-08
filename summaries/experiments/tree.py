acccb48e10b5fd9f3aee067927ff830c00a6252d
Till Hoffmann <tillahoffmann@gmail.com>
2023-08-08 10:04:06 -0400

    Remove dedicated posterior mean prediction head.

--- summaries/experiments/tree.py
+++ summaries/experiments/tree.py
@@ -186,10 +186,12 @@ def evaluate_gini(x: np.ndarray) -> float:
     return 1 - 2 / (n - 1) * (n - (1 + np.arange(n)) @ x / x.sum())
 
 
-class _TreeTransformer(NeuralTransformer):
+class TreePosteriorMeanTransformer(NeuralTransformer):
     """
     Learnable transformer for the tree kernel inference problem without the "head" of the network.
     """
+    transformer: SequentialWithKeywords
+
     def __init__(self, depth: int = 2) -> None:
         layers = []
         for _ in range(depth):
@@ -199,12 +201,20 @@ class _TreeTransformer(NeuralTransformer):
                 nn.LazyLinear(8),
                 nn.Tanh(),
             )))
-        layers.extend([nn.LazyLinear(1), nn.Tanh(), MeanPoolByGraph()])
+        layers.extend([nn.LazyLinear(1), MeanPoolByGraph()])
         transformer = SequentialWithKeywords(*layers)
         super().__init__(transformer)
 
+    def transform(self, data: Data) -> Tensor:
+        # First apply the GNN ...
+        features = torch.ones([data.num_nodes, 1])
+        return self.transformer(features, edge_index=data.edge_index, batch=data.batch)
 
-class TreePosteriorMixtureDensityTransformer(_TreeTransformer):
+    def forward(self, data: Data) -> torch.Tensor:
+        return self.transform(data)
+
+
+class TreePosteriorMixtureDensityTransformer(TreePosteriorMeanTransformer):
     """
     Learnable transformer with conditional posterior density estimation "head" based on mixture
     density networks.
@@ -214,15 +224,12 @@ class TreePosteriorMixtureDensityTransformer(_TreeTransformer):
         self.n_components = n_components
         self.mixture_parameters = nn.ModuleDict({
             key: nn.Sequential(
+                nn.Tanh(),
                 nn.LazyLinear(8),
                 nn.Tanh(),
                 nn.LazyLinear(size * n_components),
             ) for (size, key) in [(1, "logits"), (1, "concentration1s"), (1, "concentration0s")]
         })
-
-    def transform(self, data: Data) -> torch.Tensor:
-        features = torch.ones([data.num_nodes, 1])
-        return self.transformer(features, edge_index=data.edge_index, batch=data.batch)
 
     def forward(self, data: Data) -> Tensor:
         transformed = self.transform(data)
@@ -264,27 +271,3 @@ def predecessors_to_datasets(predecessors: np.ndarray, params: np.ndarray | None
 
         datasets.append(Data(edge_index=edge_index, num_nodes=n_edges + 1, **kwargs))
     return datasets
-
-
-class TreePosteriorMeanTransformer(_TreeTransformer):
-    """
-    Learnable transformer with posterior mean predictive "head".
-    """
-    def __init__(self) -> None:
-        super().__init__()
-        self.predictor = nn.Sequential(
-            nn.Tanh(),
-            nn.LazyLinear(16),
-            nn.Tanh(),
-            nn.LazyLinear(1),
-        )
-
-    def transform(self, data: Data) -> Tensor:
-        # First apply the GNN ...
-        features = torch.ones([data.num_nodes, 1])
-        transformed = self.transformer(features, edge_index=data.edge_index, batch=data.batch)
-        # ... then try to predict the mean.
-        return self.predictor(transformed)
-
-    def forward(self, data: Data) -> torch.Tensor:
-        return self.transform(data)
