af1d47fda337fc4496addf285ad5d55bcb377ac0
Till Hoffmann <tillahoffmann@gmail.com>
2025-10-09 16:38:31 -0400

    Remove task to build obsolete requirements file.

--- recipe.py
+++ recipe.py
@@ -8,9 +8,6 @@ from summaries.util import load_pickle
 from typing import Dict
 
 
-create_task("requirements", action="pip-compile -v", targets=["requirements.txt"],
-            dependencies=["requirements.in", "setup.py"])
-
 create_task("tests", action="pytest -v --cov=summaries --cov-report=html --cov-report=term-missing "
             "--cov-fail-under=100")
 
